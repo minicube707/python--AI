c03bf47caa019fd13e82a3b1d08bd4630e5da8f7
Florent Motte <fmotte@f6r5s15.clusters.42paris.fr>
2025-08-08 18:04:11 +0200

    Add the second dataset

---
+++ Convolutional Neural Network/ConvDigit/Data2/AI-Digit.py
@@ -0,0 +1,126 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import os
+
+from Preprocessing import preprocessing
+from Mathematical_function import softmax
+from File_Management import file_management, select_model
+from Deep_Learning import convolution_neuron_network
+from Initialisation_CNN import initialisation_CNN
+from Propagation import forward_propagation
+from Convolution_Neuron_Network import deshape
+from Display_parametre_CNN import display_kernel_and_biais
+
+module_dir = os.path.dirname(__file__)
+os.chdir(module_dir)
+
+load = False
+
+#Data_Digit
+with np.load("data/mnist.npz") as f:
+        X, y = f["x_train"], f["y_train"]
+
+#Initialisation CNN
+learning_rate_CNN = 0.001
+beta1 = 0.9
+beta2 = 0.999
+
+#Initialisation DNN
+hidden_layer = (128, 128)
+learning_rate_DNN = 0.0001
+
+dimensions_CNN = {}
+#Kernel size, stride, padding, nb_kernel, type layer, function
+dimensions_CNN = {  "1" :(3, 1, 0, 32, "kernel", "relu"),
+                    "2" :(2, 2, 0, 1, "pooling", "max"), 
+                    "3" :(3, 1, 0, 64, "kernel", "relu"),
+                    "4" :(2, 2, 0, 1, "pooling", "max"),
+                    "5" :(2, 1, 0, 64, "kernel", "sigmoide")}
+
+nb_iteration = 25
+
+#Number of channel by picture
+input_shape = (1, 28, 28)
+
+padding_mode = "auto"
+_, _, dimensions_CNN, _ = initialisation_CNN(input_shape, dimensions_CNN, padding_mode)
+
+X_train, y_train, X_test, y_test, transformer = preprocessing(X[:1000], y[:1000], dimensions_CNN)
+
+if load:    
+    #Load the model
+    model = select_model()
+    with open("Model/" + str(model), 'rb') as file:
+        parametres = pickle.load(file)
+
+else:   
+    parametres_CNN, parametres_DNN, dimensions_CNN, dimensions_DNN, test_accu, tuple_size_activation = convolution_neuron_network(X_train, y_train, X_test, y_test, nb_iteration, hidden_layer, dimensions_CNN \
+        , learning_rate_CNN, learning_rate_DNN, beta1, beta2, input_shape)
+
+if not load:
+
+    #Save the best model
+    name_model = file_management(test_accu)
+    print(name_model)
+    with open("Model/" + name_model, 'wb') as file:
+        pickle.dump((parametres_CNN, parametres_DNN), file)
+
+
+#display_kernel_and_biais(parametres_CNN)
+
+#______________________________________________________________#
+
+C_CNN = len(dimensions_CNN.keys())
+C_DNN = len(parametres_DNN) // 2
+
+y_final = transformer.inverse_transform(y_test)
+
+#Affichage des 15 premières images
+plt.figure(figsize=(16,8))
+for i in range(1,16):
+
+    # Prédiction des probabilités avec softmax
+    _, activation_DNN = forward_propagation(X_test[i], parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, C_CNN)
+    probabilities = softmax(activation_DNN["A" + str(C_DNN)].T)
+    pred = np.argmax(probabilities)
+    porcent = np.max(probabilities)
+
+    plt.subplot(4,5, i)
+    plt.imshow(X_test[i].reshape(8, 8), cmap="gray")
+    plt.title(f"Value:{y_final[i]} Predict:{pred}  ({np.round(porcent, 2)}%)")
+    plt.tight_layout()
+    plt.axis("off")
+plt.show()  
+
+nb_test = 10
+print("")
+for i in range(nb_test):
+    index = int(input(f"Please enter a number between 1 and {X_test.shape[0]}: "))
+    if index == index < 0:
+        break
+    
+    # Prédiction des probabilités avec softmax
+    _, activation_DNN = forward_propagation(X_test[index], parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, C_CNN)
+    probabilities = softmax(activation_DNN["A" + str(C_DNN)].T).flatten()
+    pred = np.argmax(probabilities)
+    porcent = np.max(probabilities)
+
+    # Création de la figure avec 2 sous-graphiques (image + histogramme)
+    fig, axs = plt.subplots(2, 1, figsize=(5, 7), gridspec_kw={'height_ratios': [3, 1]})
+
+    # Affichage de l'image
+    axs[0].imshow(X_test[index].reshape(8, 8), cmap="gray")
+    axs[0].set_title(f"Value:{y_final[index]} Predict:{pred} ({np.round(porcent, 2)}%)")
+    axs[0].axis("off")
+
+    # Affichage de l'histogramme des probabilités
+    axs[1].bar(range(len(probabilities)), probabilities, color="blue")
+    axs[1].set_xticks(range(len(probabilities)))
+    axs[1].set_xlabel("Classes")
+    axs[1].set_ylabel("Probability")
+    axs[1].set_ylim(0, 1)
+
+    plt.tight_layout()
+    plt.show()
---
+++ Convolutional Neural Network/ConvDigit/Data2/Deep_Learning.py
@@ -0,0 +1,97 @@
+
+import  numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from Deep_Neuron_Network import initialisation_DNN
+from Convolution_Neuron_Network import show_information, output_shape
+from Initialisation_CNN import initialisation_CNN
+from Evaluation_Metric import verification, dx_log_loss, learning_progression
+from Display_parametre_CNN import display_kernel_and_biais
+from Propagation import forward_propagation, back_propagation, update
+
+def convolution_neuron_network(X_train, y_train, X_test, y_test, nb_iteration, hidden_layer, dimensions_CNN \
+        , learning_rate_CNN, learning_rate_DNN, beta1, beta2, input_shape):
+
+    dimensions_DNN = {}
+
+    padding_mode = "auto"
+    parametres_CNN, parametres_grad, dimensions_CNN, tuple_size_activation = initialisation_CNN(input_shape, dimensions_CNN, padding_mode)
+
+    input_size = 28
+    C_CNN = len(dimensions_CNN.keys())
+
+    for val in dimensions_CNN.values():
+        o_size = output_shape(input_size, val[0], val[1], val[2])
+        input_size = o_size
+
+    dimensions_DNN = list(hidden_layer)
+    dimensions_DNN.insert(0, np.int64(np.int64(o_size**2) * np.int64(dimensions_CNN[str(C_CNN)][3])))
+    dimensions_DNN.append(y_train.shape[1])
+
+    parametres_DNN = initialisation_DNN(dimensions_DNN)
+    C_DNN = len(parametres_DNN) // 2
+    show_information(tuple_size_activation, dimensions_CNN)    
+
+    train_loss = np.array([])
+    train_accu = np.array([])
+    train_lear = np.array([])
+    test_loss = np.array([])
+    test_accu = np.array([])
+    
+
+    #Here 
+    #the activation are in different shape, that allow the cross product for more efficy
+    #the kernel are vector to do cross product
+    #the gradient are vector
+
+    for i in tqdm(range(nb_iteration)):
+        for j in range(X_train.shape[0]):
+            
+
+            activations_CNN, activation_DNN = forward_propagation(X_train[j], parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, C_CNN)
+            gradients_DNN, gradients_CNN = back_propagation(activation_DNN, activations_CNN, parametres_DNN, parametres_CNN, dimensions_CNN, tuple_size_activation, C_DNN, y_train[j])
+            parametres_CNN, parametres_DNN = update(gradients_CNN, gradients_DNN, parametres_CNN, parametres_DNN, parametres_grad, learning_rate_CNN, learning_rate_DNN, beta1, beta2, C_CNN)
+
+
+            #Train
+            nb_rand = np.random.randint(1, X_train.shape[0])
+            train_loss, train_accu = verification(X_train[nb_rand], y_train[nb_rand], activation_DNN["A" + str(C_DNN)], train_loss, train_accu, parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, C_CNN, C_DNN)
+            h = dx_log_loss(y_train[nb_rand], learning_progression(X_train[nb_rand], parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, C_CNN, C_DNN))
+            train_lear = np.append(train_lear, h)
+
+            #Test
+            nb_rand = np.random.randint(1, X_test.shape[0])
+            _, test_activation = forward_propagation(X_test[nb_rand], parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, C_CNN)
+            test_loss, test_accu = verification(X_test[nb_rand], y_test[nb_rand], test_activation["A" + str(C_DNN)], test_loss, test_accu, parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, C_CNN, C_DNN)
+
+
+
+    print(f"L'accuracy final du train_set est de {train_accu[-1]:.5f}")
+    print(f"L'accuracy final du test_set est de {test_accu[-1]:.5f}")
+
+    #Displau info of during the learing
+    plt.figure(figsize=(12,4))
+    plt.subplot(1, 3, 1)
+    plt.plot(train_loss, label="Cost function du train_set")
+    plt.plot(test_loss, label="Cost function du test_set")
+    plt.title("Fonction Cout en fonction des itérations")
+    plt.legend()
+
+    plt.subplot(1, 3, 2)
+    plt.plot(train_accu, label="Accuracy du train_set")
+    plt.plot(test_accu, label="Accuracy du test_set")
+    plt.title("L'acccuracy en fonction des itérations")
+    plt.legend()
+
+    plt.subplot(1, 3, 3)
+    plt.plot(train_lear, label="Variation de l'apprentisage")
+    plt.title("La derive de la fonction cout")
+    plt.legend()
+
+    plt.show()
+
+    #Display kernel & biais
+    #display_kernel_and_biais(parametres_CNN)
+
+    return parametres_CNN, parametres_DNN, dimensions_CNN, dimensions_DNN, test_accu[-1], tuple_size_activation
