cb0e30da152d7b044f072db46f8247fac02ba7bf
minicube707 <florentm.contact@gmail.com>
2025-10-04 02:50:01 +0200

    Update metric

--- Convolutional Neural Network/ConvDigit/System/Evaluation_Metric.py
+++ Convolutional Neural Network/ConvDigit/System/Evaluation_Metric.py
@@ -11,8 +11,7 @@ Evaluation Metrics Function
 """
 
 def dx_log_loss(y_true, y_pred):
-    epsilon = 1e-15 #Pour empecher les /0 = ?
-    return -1 / y_true.size * np.sum((y_true / y_pred + epsilon) - (1 - y_true) / (1 - y_pred + epsilon))
+    return - 1 / y_true.size * np.sum(y_true - y_pred)
 
 def activation(X, parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, dimensions_DNN, C_CNN, C_DNN):
     _, activation_DNN = forward_propagation(X, parametres_CNN, parametres_DNN, tuple_size_activation, dimensions_CNN, C_CNN, dimensions_DNN, C_DNN)
@@ -21,7 +20,7 @@ def activation(X, parametres_CNN, parametres_DNN, tuple_size_activation, dimensi
 
 def log_loss(y_true, y_pred):
     epsilon = 1e-15 #Pour empecher les log(0) = -inf
-    return  (1/y_true.size) * np.sum( -y_true * np.log(y_pred + epsilon) - (1 - y_true) * np.log(1 - y_pred + epsilon))
+    return - 1 / y_true.size * np.sum(y_true * np.log(y_pred))
 
 def accuracy_score(y_true, y_pred):
     y_true = np.asarray(y_true)
