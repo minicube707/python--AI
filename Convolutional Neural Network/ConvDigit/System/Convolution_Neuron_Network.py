23694a2d97d3dc268936e8282f9f48b1337d0a7f
minicube707 <florentm.contact@gmail.com>
2025-10-17 22:12:08 +0200

    Remove mistake back conv

--- Convolutional Neural Network/ConvDigit/System/Convolution_Neuron_Network.py
+++ Convolutional Neural Network/ConvDigit/System/Convolution_Neuron_Network.py
@@ -320,7 +320,7 @@ def back_propagation_pooling(activation, dimensions, dZ, c):
     max_indices = np.argmax(activation["A" + str(c-1)], axis=2)
 
     # Initialise le résultat avec des zéros
-    result = np.zeros_like(activation["A" + str(c-1)], dtype=np.int16)
+    result = np.zeros_like(activation["A" + str(c-1)])
 
     # Utilise un indexage avancé pour placer les valeurs maximales
     batch_indices = np.arange(activation["A" + str(c-1)].shape[0])[:, None]
@@ -375,7 +375,7 @@ def back_propagation_kernel(activation, parametres, dimensions, gradients, dZ, c
 
     #Add the result in the dictionary
     gradients["dK" + str(c)] = dK
-    gradients["db" + str(c)] = np.sum(dZ, axis=(1, 2), keepdims=True)
+    gradients["db" + str(c)] = dZ.reshape((dZ.shape[0], dZ.shape[1] * dZ.shape[2], 1))
             
     if c > 1:
         activation_fonction = parametres["f" + str(c)]
@@ -425,7 +425,7 @@ def back_propagation_CNN(activation, parametres, dimensions, dZ, tuple_size_acti
         #Remove the padding
         #Activation are in square format
         dZ = dZ[:,:tuple_size_activation[c-1], :tuple_size_activation[c-1]]
-        
+
         if parametres["l" + str(c)] == "pooling":
            dZ = back_propagation_pooling(activation, dimensions, dZ, c) 
 
@@ -525,7 +525,7 @@ numpy.array      :             the activation matrice
 def deshape(X, k_size_sqrt, stride):
 
     input_size = int(np.sqrt(X.shape[1]*X.shape[2]))
-    new_X = np.array([], dtype=np.int16)
+    new_X = np.array([])
     
     step1 = input_size // stride
     step2 = k_size_sqrt
@@ -533,9 +533,9 @@ def deshape(X, k_size_sqrt, stride):
     for i in range(0, X.shape[0]):
         for j in range(0, X.shape[1], step1):
             for k in range(0, X.shape[2], step2):
-                new_X = np.append(new_X, X[i, j:j + step1, k:k + step2].astype(np.uint16))
+                new_X = np.append(new_X, X[i, j:j + step1, k:k + step2])
 
-    new_X = new_X.reshape((X.shape[0], input_size ,input_size))
+    new_X = new_X.reshape((X.shape[0], input_size , input_size))
     return new_X
 
 
