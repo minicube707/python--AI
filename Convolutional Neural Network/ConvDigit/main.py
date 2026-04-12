cd22595822d420f7c48e7366eca17d5a831a4556
Workstazziooooneeeee <Worksazionne@42.ai>
2026-04-12 21:51:16 +0200

    Create main.py

--- Convolutional Neural Network/ConvDigit/main.py
+++ Convolutional Neural Network/ConvDigit/main.py
@@ -1,8 +1,9 @@
 
 import os
 
-from System.Evaluation_Metric import CrossEntropyLoss, BinaryCrossEntropy
-from System.Mathematical_function import Softmax, Sigmoide
+from System.Evaluation_Metric import CrossEntropyLoss, BinaryCrossEntropy, MSE
+from System.Mathematical_function import Softmax, Sigmoide, Linear
+
 from System.Optimizer import Adam
 from System.Run_model import run_training_pipeline
 from System.Dataclasses import Hyperparams, Dataset
@@ -15,8 +16,8 @@ os.chdir(module_dir)
 # ============================
 
 hyperparams = {
-    "nb_epoch": 5,
-    "batch_size": 2,
+    "nb_epoch": 50,
+    "batch_size": 128,
 
     "lr": 0.001,
     "beta1": 0.9,
@@ -27,12 +28,12 @@ hyperparams = {
 
     "padding_mode": "auto",
 
-    "support": "CPU"
+    "support": "GPU"
 }
 
 dataset = {
     "validation_size" : 1_000,
-    "validation_frequency" : 50,
+    "validation_frequency" : -1,
 
     "dataset_size": -1,
     "ratio_test" : 0.2,
@@ -45,12 +46,13 @@ dataset = {
 
 # Structure CNN : (kernel size, stride, padding, nb kernels, type_layer, activations function)
 structure_CNN = {
-    "1": (5, 1, 0, 32, "conv", "leaky relu", 0.0),
-    "2": (2, 2, 0, 1, "pool", "max", 0.0),
-    "3": (3, 1, 0, 64, "conv", "leaky relu", 0.0),
-    "4": (2, 2, 0, 1, "pool", "max", 0.0),
-    "5": (3, 1, 0, 64, "conv", "leaky relu", 0.0)
-}
+        "1": (5, 1, 0, 32, "conv", "leaky relu", 0.0),
+        "2": (2, 2, 0, 1, "pool", "max", 0.0),
+        "3": (3, 1, 0, 64, "conv", "leaky relu", 0.0),
+        "4": (2, 2, 0, 1, "pool", "max", 0.0),
+        "5": (3, 1, 0, 128, "conv", "leaky relu", 0.0),
+        "6": (2, 2, 0, 1, "pool", "max", 0.0),
+    }
 
 
 # ============================
@@ -59,22 +61,20 @@ structure_CNN = {
 
 # Structure DNN : (number of neurone, activations function) 
 structure_DNN = {
-    "1": (128, "leaky relu", 0.2),
-    "2": (64, "leaky relu", 0.2),
-    "3": (64, "leaky relu", 0.2),
-    "4": (0,  "leaky relu", 0.2)
+    "1": (128, "leaky relu", 0.0),
+    "2": (64, "leaky relu", 0.0),
+    "3": (0,  "leaky relu", 0.0)
 }
 
 hyperparams = Hyperparams(**hyperparams)
 
 loss_metric = CrossEntropyLoss()
 output_layer = Softmax()    
-optimizer = Adam(hyperparams)
+optimizer = Adam()
 
 hyperparams.add_training_parameters(loss_metric, output_layer, optimizer)
 
 dataset = Dataset(**dataset)
 structure = (structure_CNN, structure_DNN)
-
 
 run_training_pipeline(module_dir, hyperparams, structure, loss_metric, output_layer, optimizer, dataset)
