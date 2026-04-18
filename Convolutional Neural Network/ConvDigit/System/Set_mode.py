cc346d3a68cbde28f662929bc999e3faa8505ff1
Workstazziooooneeeee <Worksazionne@42.ai>
2026-04-18 22:18:13 +0200

    CNN - Set_mode.py Add delete mode

--- Convolutional Neural Network/ConvDigit/System/Set_mode.py
+++ Convolutional Neural Network/ConvDigit/System/Set_mode.py
@@ -7,32 +7,33 @@ def set_mode():
         print("2: Fine-tuning a train model")
         print("3: Test a model")
         print("4: Examine a model")
-        str_answer = input("What do you want to do ?\n")
-        try:
-            int_answer = int(str_answer)
-        except:
-            print("Please enter only 1, 2, 3 or 4")
-            continue
+        print("5: Delete a model")
         
-        if (int_answer == 0):
+        str_answer = input("What do you want to do ?\n").strip()
+                
+        if (str_answer == "0"):
             print("Exit")
             exit(0)
 
-        if (int_answer == 1):
+        if (str_answer == "1"):
             print("You chose the mode Traning")
             return(1)
         
-        elif (int_answer == 2):
+        elif (str_answer == "2"):
             print("You chose the mode fine-tunning")
             return(2)
         
-        elif (int_answer == 3):
+        elif (str_answer == "3"):
             print("You chose the mode test")
             return(3)
         
-        elif (int_answer == 4):
+        elif (str_answer == "4"):
             print("You chose the mode test")
             return(4)
         
+        elif (str_answer == "5"):
+            print("You chose the mode delete")
+            return(5)
+        
         else:
-            print("Please enter only 1, 2, 3 or 4")
+            print("Please enter only 1, 2, 3, 4 or 5")
